11fa4e3c53950c356801db453bbab167273dd43b
Wi1mer <wilmerdiazolaya@gmail.com.com>
2021-05-04 04:40:59 -0500

    V-1.2

--- src/index.js
+++ src/index.js
@@ -9,7 +9,8 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 // middlewares
-app.use(morgan('dev'));
+app.use(morgan('dev',));
+app.use(morgan(':remote-user :date :remote-addr :user-agent :res[content-length]'));
 
 // routes
 app.use(require('./routes'));
